1c3bdeac17aa3ee81086ae795f73933836e49dd5
Owais <abc@gmail.com>
2025-04-13 02:17:40 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    if (task.trim() === '') return;
+    setTasks([task, ...tasks]);
+    setTask('');
+  };
+
+  const deleteTask = (del) => {
+    const updatedTask = tasks.filter((_, i) => i !== del);
+    setTasks(updatedTask);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container py-5">
+      <h1 className="text-center mb-4">📝 Todo App</h1>
+
+      <div className="mb-3 d-flex gap-2">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter your Task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className="btn btn-primary" onClick={handleAddTask}>
+          Add Task
+        </button>
+      </div>
+
+      <div style={{ maxHeight: '230px', overflowY: 'auto' }}>
+        <ul className="list-group">
+          {tasks.map((t, i) => (
+            <li
+              key={i}
+              className="list-group-item d-flex justify-content-between align-items-center"
+            >
+              {t}
+              <button className="btn btn-sm btn-danger" onClick={() => deleteTask(i)}>
+                Delete Task
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
